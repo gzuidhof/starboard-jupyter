dd5e2f02cd4aa062918e62d737c39ee03c954cbd
Guido Zuidhof <guido@starboard.gg>
2021-04-08 14:22:44 +0100

    Set up basic project

---
+++ src/index.ts
@@ -0,0 +1,94 @@
+import { CellTypeDefinition, CellHandlerAttachParameters, CellElements, Cell } from "starboard-notebook/dist/src/types";
+import * as lithtmlImport from "lit-html";
+import { Runtime, ControlButton } from "starboard-notebook/dist/src/runtime";
+
+declare global {
+    interface Window {
+      runtime: Runtime
+      $_: any;
+    }
+}
+
+export function registerPython() {
+    /* These globals are exposed by Starboard Notebook. We can re-use them so we don't have to bundle them again. */
+    const runtime = window.runtime;
+    const lithtml = runtime.exports.libraries.LitHtml;
+
+    const StarboardTextEditor = runtime.exports.elements.StarboardTextEditor;
+    const cellControlsTemplate = runtime.exports.templates.cellControls;
+    const icons = runtime.exports.templates.icons;
+
+    const JUPYTER_CELL_TYPE_DEFINITION: CellTypeDefinition = {
+        name: "Jupyter",
+        // @ts-ignore Ignore to be removed after updating typings.
+        cellType: ["jupyter"],
+        createHandler: (cell: Cell, runtime: Runtime) => new JupyterCellHandler(cell, runtime),
+    }
+
+    class JupyterCellHandler {
+        private elements!: CellElements;
+        private editor: any;
+
+        private lastRunId = 0;
+        private isCurrentlyRunning: boolean = false;
+
+        cell: Cell;
+        runtime: Runtime;
+
+        constructor(cell: Cell, runtime: Runtime) {
+            this.cell = cell;
+            this.runtime = runtime;
+        }
+
+        private getControls(): lithtmlImport.TemplateResult | string {
+            const icon = this.isCurrentlyRunning ? icons.ClockIcon : icons.PlayCircleIcon;
+            const tooltip = this.isCurrentlyRunning ? "Run Cell": "Cell is running";
+            const runButton: ControlButton = {
+                icon,
+                tooltip,
+                callback: () => this.runtime.controls.emit({id: this.cell.id, type: "RUN_CELL"}),
+            };
+            let buttons = [runButton];
+
+            return cellControlsTemplate({ buttons });
+        }
+
+        attach(params: CellHandlerAttachParameters): void {
+            this.elements = params.elements;
+
+            const topElement = this.elements.topElement;
+            lithtml.render(this.getControls(), this.elements.topControlsElement);
+
+            this.editor = new StarboardTextEditor(this.cell, this.runtime, {language: "python"});
+            topElement.appendChild(this.editor);
+        }
+
+        async run() {
+            const codeToRun = this.cell.textContent;
+
+            this.lastRunId++;
+            const currentRunId = this.lastRunId;
+            this.isCurrentlyRunning = true;
+
+            const val = 0;
+            // const val = await runStarboardPython(this.runtime, codeToRun, this.elements.bottomElement);
+            if (this.lastRunId === currentRunId) {
+                this.isCurrentlyRunning = false;
+                lithtml.render(this.getControls(), this.elements.topControlsElement);
+            }
+
+            return val;
+        }
+
+        focusEditor() {
+            this.editor.focus();
+        }
+
+        async dispose() {
+            this.editor.remove();
+        }
+    
+    }
+
+    runtime.definitions.cellTypes.register(JUPYTER_CELL_TYPE_DEFINITION.cellType, JUPYTER_CELL_TYPE_DEFINITION);
+}
